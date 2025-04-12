458615828948fc2eeea5673a241a02451e4640ff
Amro <amrotrteer@gmail.com>
2025-04-12 19:11:56 +0300

    commited

---
+++ src/config/firbaseConfig.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAIr7HfC8TPWAv_DaFx5uQbnCF4xyPX2gI",
+  authDomain: "henini-prj.firebaseapp.com",
+  projectId: "henini-prj",
+  storageBucket: "henini-prj.firebasestorage.app",
+  messagingSenderId: "702193062188",
+  appId: "1:702193062188:web:8d7220d07ab17ef48a1c89",
+  measurementId: "G-5JHE2J4RP2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export {app};
