14b65075318594e8d33b31c94b6ca211f0c9bd60
Laith Mimi <laithmimi03@gmail.com>
2025-05-31 19:13:13 +0300

    Revert "main"
    This reverts commit 8e20ce06c2d56f5d2c8c258e722c4c45e49f68f9.

--- src/pages/admin/Users.jsx
+++ src/pages/admin/Users.jsx
@@ -90,14 +90,15 @@ await deleteDoc(doc(db, "conversations", uid));
   }
 
   // Delete mentorships where user is a mentor or participant
-  const mentorQuery = query(collection(db, "users"), where("mentors", "array-contains", uid));
+  const mentorQuery = query(collection(db, "mentorship"), where("mentorId", "==", uid));
   const mentorSnapshot = await getDocs(mentorQuery);
-  for (const userDoc of mentorSnapshot.docs) {
-    const userData = userDoc.data();
-    const updatedMentors = userData.mentors.filter(mentorId => mentorId !== uid);
-    await updateDoc(doc(db, "users", userDoc.id), {
-      mentors: updatedMentors
-    });
+  for (const msDoc of mentorSnapshot.docs) {
+    await deleteDoc(doc(db, "mentorship", msDoc.id));
+  }
+  const participantQuery = query(collection(db, "mentorship"), where("participantId", "==", uid));
+  const participantSnapshot = await getDocs(participantQuery);
+  for (const msDoc of participantSnapshot.docs) {
+    await deleteDoc(doc(db, "mentorship", msDoc.id));
   }
 }
 
